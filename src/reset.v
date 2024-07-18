496b7109c9bf749f214b39f9d03858cdeb94758a
YoursHugo <1211852956@qq.com>
2024-07-18 13:03:45 +0800

    refactor: Refactor reset module for state machine

--- src/reset.v
+++ src/reset.v
@@ -7,48 +7,49 @@ module reset(
     output reg onfi_dqs_en,
     output reg [31:0] onfi_dq_o,
     output reg onfi_dq_en
-    );
-    initial begin
-        onfi_cen = 0;
-        onfi_cle = 0;
-        onfi_ale = 0;
-        onfi_wen = 0;
-        onfi_dqs_en = 0;
-        onfi_dq_o = 32'h00;
-        onfi_dq_en = 0;
-    end
-    reg [1:0] state=0;
-    always@(negedge onfi_clk)
-    switch(state)
-    case 0:
-     begin
-        assign onfi_cen = 0;
-        assign onfi_cle = 0;
-        assign onfi_ale = 0;
-        assign onfi_wen = 1;
-        assign onfi_dqs_en = 0;
-        state++;
-        break;
-    end
-    case 1: 
-     begin
-        assign onfi_cle = 1;
-        onfi_dq_o = 32'hFF;
-        assign onfi_dq_en = 1;
-        state++;
-        break;
-    end
-    case 2:
-     begin
-        assign onfi_cle = 0;
-        assign onfi_dq_en = 0;
-        state++;
-        break;
-    end
-    case 4: 
-     begin
-        assign onfi_cen = 1;
-        state=0;
-        break;
-    end
+);
+
+initial begin
+    onfi_cen = 0;
+    onfi_cle = 0;
+    onfi_ale = 0;
+    onfi_wen = 0;
+    onfi_dqs_en = 0;
+    onfi_dq_o = 32'h00;
+    onfi_dq_en = 0;
+end
+
+reg [1:0] state = 0;
+
+always @(negedge onfi_clk) begin
+    case (state)
+        0: begin
+            onfi_cen = 0;
+            onfi_cle = 0;
+            onfi_ale = 0;
+            onfi_wen = 1;
+            onfi_dqs_en = 0;
+            state = state + 1;
+        end
+        1: begin
+            onfi_cle = 1;
+            onfi_dq_o = 32'hFF;
+            onfi_dq_en = 1;
+            state = state + 1;
+        end
+        2: begin
+            onfi_cle = 0;
+            onfi_dq_en = 0;
+            state = state + 1;
+        end
+        3: begin
+            onfi_cen = 1;
+            state = 0;
+        end
+        default: begin
+            state = 0;
+        end
+    endcase
+end
+
 endmodule
